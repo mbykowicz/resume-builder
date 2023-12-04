76c73d8cb6c88e15bcf3692014dd4ae35fb7e095
Mateusz Bykowicz <mbykowicz@gmail.com>
2023-12-04 18:34:25 +0100

    feat(ui): add button

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -3,13 +3,50 @@ import { cva, type VariantProps } from 'class-variance-authority'
 import { cn } from '@/lib/utils'
 import { Slot } from '@radix-ui/react-slot'
 
-type Props = {
-  children?: React.ReactNode
-}
-export type Ref = HTMLButtonElement
+const variants = cva(
+  'inline-flex justify-center items-center font-medium rounded-full border transition disabled:text-disabled-foreground focus-visible:outline-none focus-visible:ring focus-visible:ring-ring/25',
+  {
+    variants: {
+      variant: {
+        default:
+          'bg-zinc-900 text-white border-transparent hover:bg-zinc-800 disabled:bg-disabled-background',
+        secondary:
+          'bg-white text-zinc-900 border-border hover:bg-zinc-100 disabled:bg-disabled-background disabled:bored-disabled-border',
+        ghost:
+          'bg-transparent text-zinc-900 border-transparent hover:border-border',
+        link: 'bg-transparent text-zinc-900 hover:text-zinc-800',
+        destructive:
+          'bg-destructive text-destructive-foreground border-transparent hover:bg-destructive/90 focus-visible:ring-destructive/30',
+      },
+      size: {
+        default: 'h-10 px-6',
+        small: 'h-8 px-6',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+      size: 'default',
+    },
+  },
+)
 
-const Button = React.forwardRef<Ref, Props>(({ children }, ref) => {
-  return <button ref={ref}>{children}</button>
-})
+interface Props
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof variants> {
+  asChild?: boolean
+}
+
+const Button = React.forwardRef<HTMLButtonElement, Props>(
+  ({ asChild = false, className, variant, size, ...props }, ref) => {
+    const Component = asChild ? Slot : 'button'
+    return (
+      <Component
+        className={cn(variants({ variant, size, className }))}
+        ref={ref}
+        {...props}
+      />
+    )
+  },
+)
 Button.displayName = 'Button'
 export default Button
--- stories/Button.stories.tsx
+++ stories/Button.stories.tsx
@@ -1,21 +1,50 @@
-// import type { Meta, StoryObj } from '@storybook/react'
+import type { Meta, StoryObj } from '@storybook/react'
 
-// import { Button } from '@/components/shadcn/ui/button'
+import Button from '@/components/ui/Button'
 
-// const meta = {
-//   title: 'UI/Button',
-//   component: Button,
-//   parameters: {
-//     layout: 'centered',
-//   },
-//   render: (args) => <Button {...args}>Button</Button>,
-// } satisfies Meta<typeof Button>
+const meta = {
+  title: 'UI/Button',
+  component: Button,
+  parameters: {
+    layout: 'centered',
+  },
+  render: (args) => <Button {...args}>Button</Button>,
+} satisfies Meta<typeof Button>
 
-// export default meta
-// type Story = StoryObj<typeof meta>
+export default meta
+type Story = StoryObj<typeof meta>
 
-// export const Primary: Story = {
-//   args: {
-//     variant: 'default',
-//   },
-// }
+export const Default: Story = {
+  args: {
+    variant: 'default',
+    size: 'default',
+  },
+}
+
+export const Secondary: Story = {
+  args: {
+    variant: 'secondary',
+    size: 'default',
+  },
+}
+
+export const Ghost: Story = {
+  args: {
+    variant: 'ghost',
+    size: 'default',
+  },
+}
+
+export const Link: Story = {
+  args: {
+    variant: 'link',
+    size: 'default',
+  },
+}
+
+export const Destructive: Story = {
+  args: {
+    variant: 'destructive',
+    size: 'default',
+  },
+}
