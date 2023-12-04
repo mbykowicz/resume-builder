2db593ff1d1374acfead1ae40696d181dd28a403
Mateusz Bykowicz <mbykowicz@gmail.com>
2023-12-04 16:10:14 +0100

    chore: delete all shadcn components; change shadcn settings

--- stories/Button.stories.tsx
+++ stories/Button.stories.tsx
@@ -1,33 +1,21 @@
-import type { Meta, StoryObj } from '@storybook/react'
+// import type { Meta, StoryObj } from '@storybook/react'
 
-import { Button } from '@/components/ui/button'
+// import { Button } from '@/components/shadcn/ui/button'
 
-const meta = {
-  title: 'UI/Button',
-  component: Button,
-  parameters: {
-    layout: 'centered',
-  },
-  render: (args) => <Button {...args}>Button</Button>,
-} satisfies Meta<typeof Button>
+// const meta = {
+//   title: 'UI/Button',
+//   component: Button,
+//   parameters: {
+//     layout: 'centered',
+//   },
+//   render: (args) => <Button {...args}>Button</Button>,
+// } satisfies Meta<typeof Button>
 
-export default meta
-type Story = StoryObj<typeof meta>
+// export default meta
+// type Story = StoryObj<typeof meta>
 
-export const Primary: Story = {
-  args: {
-    variant: 'default',
-  },
-}
-
-export const Secondary: Story = {
-  args: {
-    variant: 'secondary',
-  },
-}
-
-export const Outline: Story = {
-  args: {
-    variant: 'outline',
-  },
-}
+// export const Primary: Story = {
+//   args: {
+//     variant: 'default',
+//   },
+// }
